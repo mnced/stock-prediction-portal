8be98ff96d25df8a137614beb4d6e0f6b2a1d855
mnced <mncemyo@gmail.com>
2026-09-16 20:18:07 +0200

    registration-backend-api

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "rest_framework",
+    "api",
+    "accounts",
     'django.contrib.staticfiles',
 ]
 
